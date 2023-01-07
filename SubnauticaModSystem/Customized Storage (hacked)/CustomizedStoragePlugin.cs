c8bc34e3d76ea2f6f02c03671a3b4f6a76e4e6a1
MMZTimeLord <48738604+MMZTimeLord@users.noreply.github.com>
2023-01-07 11:39:04 -0600

    CustomizedStorage 1.0.7 (bugfix)

--- SubnauticaModSystem/Customized Storage (hacked)/CustomizedStoragePlugin.cs
+++ SubnauticaModSystem/Customized Storage (hacked)/CustomizedStoragePlugin.cs
@@ -4,22 +4,22 @@ using HarmonyLib;
 
 namespace CustomizedStorage
 {
-    [BepInPlugin(GUID, MODNAME, VERSION)]
+    [BepInPlugin(myGUID, MODNAME, VERSION)]
     public class CustomizedStorage : BaseUnityPlugin
     {
-        
+
         // Mod Information: Edit the size/capacity of any container in the game, including the player inventory!
         // Original Base Code by RandyKnapp - Updated for BepInEx by d2allgr
         public const string
             MODNAME = "CustomizedStorage",
             AUTHOR = "RandyKnapp",
-            GUID = AUTHOR + "_" + MODNAME,
+            myGUID = "com." + AUTHOR + "." + MODNAME,
             VERSION = "1.0.6.0";
-        
+
         private void Awake()
         {
             Mod.LoadConfig();
-            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), GUID);
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), myGUID);
             Logger.LogInfo(MODNAME + " " + VERSION + " " + "loaded.");
         }
     }
